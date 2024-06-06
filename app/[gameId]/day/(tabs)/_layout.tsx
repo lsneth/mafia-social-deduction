848615418c0a8354bee859c813befa398c35cc5c
Lucas Nethercott <luke@nethercott.com>
2024-06-06 11:20:01 -0600

    add bare minimum for day tabs

--- app/[gameId]/day/(tabs)/_layout.tsx
+++ app/[gameId]/day/(tabs)/_layout.tsx
@@ -1,11 +1,39 @@
 import { Tabs } from 'expo-router'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="history" />
-      <Tabs.Screen name="role" />
+    <Tabs
+      screenOptions={{
+        headerTitle: () => <></>,
+        headerTransparent: true,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="supervisor-account" color={color} />,
+          tabBarShowLabel: false,
+          tabBarAccessibilityLabel: 'Day',
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="history" color={color} />,
+          tabBarShowLabel: false,
+          tabBarAccessibilityLabel: 'History',
+        }}
+      />
+      <Tabs.Screen
+        name="role"
+        options={{
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="person" color={color} />,
+          tabBarShowLabel: false,
+          tabBarAccessibilityLabel: 'Role',
+          // TODO: use `tabBarButton` with a pressable that has `onPressIn` and `onPressOut` to only show the role while holding down the tab
+        }}
+      />
     </Tabs>
   )
 }
