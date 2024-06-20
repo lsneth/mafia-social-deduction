fa161e8db1852b1413144f8e58d68d175cca406d
Lucas Nethercott <luke@nethercott.com>
2024-06-20 07:12:16 -0600

    ga debugging 5

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -7,8 +7,8 @@ Cypress.Commands.add(
   'signIn',
   (
     credentials = {
-      email: '1@test.com',
-      password: '123456',
+      email: Cypress.env('AUTOMATED_TESTING_EMAIL'),
+      password: Cypress.env('AUTOMATED_TESTING_PASSWORD'),
     }
   ) => {
     cy.task('signIn', credentials).then((sessionData) => {
