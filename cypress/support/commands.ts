f715ca912a3cd61961d2c13e2125515f95f31252
Lucas Nethercott <luke@nethercott.com>
2024-06-19 23:01:57 -0600

    hopefully last env var change haha

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -6,7 +6,10 @@ export {}
 Cypress.Commands.add(
   'signIn',
   (
-    credentials = { email: Cypress.env('AUTOMATED_TESTING_EMAIL'), password: Cypress.env('AUTOMATED_TESTING_PASSWORD') }
+    credentials = {
+      email: Cypress.env('CYPRESS_AUTOMATED_TESTING_EMAIL'),
+      password: Cypress.env('CYPRESS_AUTOMATED_TESTING_PASSWORD'),
+    }
   ) => {
     cy.task('signIn', credentials).then((sessionData) => {
       localStorage.setItem('sb-krsvqfsdxblshgkwnwnb-auth-token', JSON.stringify(sessionData))
