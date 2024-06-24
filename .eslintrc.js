bc8f2a02d6c70e5cf91f92e1fd84408778ae4fcf
Lucas Nethercott <luke@nethercott.com>
2024-06-24 11:34:52 -0600

    add eslint error for .only and .skip in tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
   extends: ['expo', 'plugin:cypress/recommended'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'error',
+    'no-only-tests/no-only-tests': [
+      'error',
+      {
+        focus: ['only', 'skip'],
+      },
+    ],
   },
-  plugins: ['cypress'],
+  plugins: ['cypress', 'no-only-tests'],
   ignorePatterns: ['dist/**'],
 }
